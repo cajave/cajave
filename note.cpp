9a11462786a58c0029a18d3b1742fedffa2f6048
caojave <caojave@126.com>
2019-08-18 05:11:05 +0800

    加#

--- note.cpp
+++ note.cpp
@@ -1,12 +1,12 @@
-echo "# cajave" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:cajave/cajave.git
-git push -u origin master
-
-git remote add origin git@github.com:cajave/cajave.git
-git push -u origin master
+//echo "# cajave" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git remote add origin git@github.com:cajave/cajave.git
+//git push -u origin master
+//
+//git remote add origin git@github.com:cajave/cajave.git
+//git push -u origin master
 
 #define DSTKEY "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Windows"
 RegOpenKeyEx(HKEY_LOCAL_MACHINE,
