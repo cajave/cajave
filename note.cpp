0c896bc21a9d3fc13f9bb2e6d8b1283778acc629
caojave <caojave@126.com>
2019-08-18 05:14:15 +0800

    删除重复的两行

--- note.cpp
+++ note.cpp
@@ -4,9 +4,6 @@
 //git commit -m "first commit"
 //git remote add origin git@github.com:cajave/cajave.git
 //git push -u origin master
-//
-//git remote add origin git@github.com:cajave/cajave.git
-//git push -u origin master
 
 #define DSTKEY "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Windows"
 RegOpenKeyEx(HKEY_LOCAL_MACHINE,
