e199a3b2785d9d9ff24ebb729d6e29a1f4285976
cajave <30372025+cajave@users.noreply.github.com>
2019-09-18 14:45:50 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -3,5 +3,6 @@ $* $1$2$3...
 $0 脚本程序名称
 $? 上一个命令的返回码
 $# 参数个数
-
-stat filename|grep Modify|awk '{print $2}'|sed s/-//g #获取文件的修改日期
+#stat filename | grep Modify => Modify: 2019-09-18 10:05:55.526576976 +000
+stat filename | grep Modify | awk '{print $2}'|sed s/-//g #20190918
+stat filename | grep Modify | awk '{print $2 $3}' | cut -d"." -f1 | sed -e 's/-//g' -e 's/://g' #20190918100555
